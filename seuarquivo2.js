52c18d1c995e54fab38af6d1eb162e7e3f46aff6
bielfigs <148137388+bielfigs@users.noreply.github.com>
2023-12-07 00:42:52 -0300

    Delete seuarquivo2.js

--- seuarquivo2.js
+++
@@ -1,9 +0,0 @@
-function somatorio(numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-    }
-    return sum;
-  }
-  
-  module.exports = { somatorio };
